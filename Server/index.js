aa89a148993309567632fa0f7636d6ef93b87d7a
or-nob <ishraqarnob@gmail.com>
2020-07-26 01:09:37 +0600

    First Commit

---
+++ Server/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const { json } = require('express');
+const app = express();
+
+app.use(cors());
+
+app.listen(4000, () => {
+    console.log('Server Works !!! At port 4000');
+});
+
+async function getTitle(id){
+    return await ytdl.getInfo(id).then(info => {
+        return JSON.stringify(
+            [
+                {'title': info.videoDetails.title},
+                {'rating': info.player_response.videoDetails.averageRating},
+                {'uploaded by': info.videoDetails.author.name}
+            ]
+        );
+      }).catch(err=>err);
+}
+
+app.get('/download', async(req,res) => {
+    var URL = req.query.URL;
+    var index = URL.toString().indexOf("v=");
+    index+=2;
+    var id = URL.slice(index,URL.toString().length);
+
+    var details = await getTitle(id);
+    var JsonDetails = JSON.parse(details);
+
+    res.header('Content-Disposition', 'attachment; filename="'+JsonDetails[0].title+'.mp4"');
+    ytdl(URL, {
+        format: 'mp4'
+        }).pipe(res);
+    }
+);
