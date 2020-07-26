a870ff991758c161ed9b1795815aa43d487904b9
or-nob <ishraqarnob@gmail.com>
2020-07-26 19:18:14 +0600

    Recoded the functions to conform to async/await formula

--- Server/index.js
+++ Server/index.js
@@ -11,29 +11,38 @@ app.listen(4000, () => {
 });
 
 async function getTitle(id){
-    return await ytdl.getInfo(id).then(info => {
-        return JSON.stringify(
-            [
-                {'title': info.videoDetails.title},
-                {'rating': info.player_response.videoDetails.averageRating},
-                {'uploaded by': info.videoDetails.author.name}
-            ]
-        );
-      }).catch(err=>err);
+    try{
+        const info = await ytdl.getInfo(id);
+        const details = JSON.stringify(
+                [
+                    {'title': info.videoDetails.title},
+                    {'rating': info.player_response.videoDetails.averageRating},
+                    {'uploaded by': info.videoDetails.author.name}
+                ]
+            );
+        return details;
+    }catch(err){
+        throw err;
+    }
 }
 
 app.get('/download', async(req,res) => {
-    var URL = req.query.URL;
-    var index = URL.toString().indexOf("v=");
-    index+=2;
-    var id = URL.slice(index,URL.toString().length);
+        try{
+            var URL = req.query.URL;
+            var index = URL.toString().indexOf("v=");
+            index+=2;
+            var id = URL.slice(index,URL.toString().length);
 
-    var details = await getTitle(id);
-    var JsonDetails = JSON.parse(details);
+            var details = await getTitle(id);
+            var JsonDetails = JSON.parse(details);
 
-    res.header('Content-Disposition', 'attachment; filename="'+JsonDetails[0].title+'.mp4"');
-    ytdl(URL, {
-        format: 'mp4'
-        }).pipe(res);
+            res.header('Content-Disposition', 'attachment; filename="'+JsonDetails[0].title+'.mp4"');
+            ytdl(URL, {
+                format: 'mp4'
+                }).pipe(res);
+        }catch(err){
+
+        }
     }
 );
+    
